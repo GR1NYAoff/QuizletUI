f6fc3ddfe33de095fa53b5fd55fe0d268620bc4b
= <=>
2022-04-22 00:18:17 +0300

    update routing pathes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { TestComponent } from './components/test/test.component';
 import { TestsListComponent } from './components/tests-list/tests-list.component';
 //add TestComponent
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'test', component: TestsListComponent}
+//{path: 'tests/', component: TestComponent},
+//{path: 'tests', component: TestsListComponent}
 ];
 
 @NgModule({
