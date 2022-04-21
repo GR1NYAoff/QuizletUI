fe321b1facb046e9113c96a74bb460ac97a4c7e0
= <=>
2022-04-21 22:06:05 +0300

    add lib @auth0/angular-jwt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,26 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
 import {MatFormFieldModule} from '@angular/material/form-field';
+
+import { JwtModule } from '@auth0/angular-jwt'
+
 import { HomeComponent } from './components/home/home.component';
 import { TestsListComponent } from './components/tests-list/tests-list.component';
 import { TestComponent } from './components/test/test.component'
 import { AUTH_API_URL } from './app-injection-tokens';
 import { environment } from 'src/environments/environment';
+import { ACCESS_TOKEN_KEY } from './services/auth.service';
+
+export function tokenGetter() {
+  return localStorage.getItem(ACCESS_TOKEN_KEY);
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     TestsListComponent,
-    TestComponent
+    TestComponent,
   ],
   imports: [
     BrowserModule,
@@ -34,7 +42,14 @@ import { environment } from 'src/environments/environment';
     MatInputModule,
     MatButtonModule,
     MatTableModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+
+    JwtModule.forRoot({
+      config: {
+        tokenGetter,
+        disallowedRoutes: environment.tokenDisallowedRoutes
+      }
+    })
   ],
   providers: [{
     provide: AUTH_API_URL,
