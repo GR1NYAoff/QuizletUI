d136f20d72ae4d507d6dae4aae6b96881438e2d3
= <=>
2022-04-22 00:16:30 +0300

    auth implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
-  title = 'Angular13-QuizletUI';
+  public get isLoggedIn(): boolean {
+    return this.as.isAuthenticated();
+  }
+
+  constructor(private as: AuthService) {}
+
+  login(email: string, password: string) {
+    this.as.login(email, password).subscribe(
+      (res) => {},
+      (error) => {
+        alert('Wrang email or password');
+      }
+    );
+  }
+
+  logout() {
+    this.as.logout();
+  }
 }
