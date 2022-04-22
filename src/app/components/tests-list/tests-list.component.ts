cfadb4681d49b4679be9ff7d5381fc17f689fdcf
= <=>
2022-04-22 14:45:14 +0300

    get test, get tests implemented

--- src/app/components/tests-list/tests-list.component.ts
+++ src/app/components/tests-list/tests-list.component.ts
@@ -1,33 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Test } from 'src/app/Models/test';
 import { TestsService } from 'src/app/services/tests.service';
-import { ACCESS_TOKEN_KEY } from 'src/app/services/auth.service';
+
 @Component({
   selector: 'app-tests-list',
   templateUrl: './tests-list.component.html',
   styleUrls: ['./tests-list.component.scss'],
 })
 export class TestsListComponent implements OnInit {
-  tests: Test[] = [];
-  test: Test;
+  
+  test:Test;
+  list:Test[];
 
   constructor(private ts: TestsService) {}
 
   ngOnInit(): void {
     this.refreshList();
-    
+    console.log(this.list);
   }
 
   refreshList() {
     this.ts.getAvailableTests().subscribe((res) => {
-      this.tests = res;
+      this.list = res as Test[];
       console.log(res);
     });
   }
 
   getTest(id: number) {
     this.ts.getTest(id).subscribe((res) => {
-      this.test = res;
+      this.test = res as Test;
     });
   }
 }
