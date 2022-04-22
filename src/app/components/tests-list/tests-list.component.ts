97b6f7e69ca1f21eb50707cc7659b67a1f4d44f5
= <=>
2022-04-22 09:48:17 +0300

    update app components

--- src/app/components/tests-list/tests-list.component.ts
+++ src/app/components/tests-list/tests-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Test } from 'src/app/Models/test';
+import { TestsService } from 'src/app/services/tests.service';
 @Component({
   selector: 'app-tests-list',
   templateUrl: './tests-list.component.html',
-  styleUrls: ['./tests-list.component.scss']
+  styleUrls: ['./tests-list.component.scss'],
 })
 export class TestsListComponent implements OnInit {
+  tests: Test[] = [];
+  test: Test;
 
-  constructor() { }
+  constructor(private ts: TestsService) {}
 
   ngOnInit(): void {
+    this.refreshList();
   }
 
+  refreshList(){
+    this.ts.getAvailableTests().subscribe((res) => {
+      this.tests = res;
+    });
+  }
+
+  getTest(id: number){
+    this.ts.getTest(id).subscribe(res => {
+      this.test = res;
+    });
+  }
 }
