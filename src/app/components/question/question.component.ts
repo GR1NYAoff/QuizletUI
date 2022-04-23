064dae5a8f0f3673bb5399b276425282213a7f75
= <=>
2022-04-23 21:18:07 +0300

    implement getting receiving answers & questions from LS

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Answer } from 'src/app/Models/answer.';
+import { Question } from 'src/app/Models/question';
+import { Test } from 'src/app/Models/test';
+import { CURRENT_TEST } from 'src/app/services/tests.service';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+  styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  questionList: Question[] = [];
+  answerList: Answer[] = [];
 
-  ngOnInit(): void {
+  currentQuestion: number = 0;
+  test: Test;
+
+  getAllQuestions(): Question[] {
+    return JSON.parse(this.getCurrentTest().questions);
   }
 
+  getAllAnswers(): Answer[] {
+    return JSON.parse(this.getCurrentTest().answers);
+  }
+
+  getCurrentTest(): Test {
+    var jsonTest = localStorage.getItem(CURRENT_TEST);
+    return JSON.parse(jsonTest!);
+  }
+
+  ngOnInit(): void {
+    this.questionList = this.getAllQuestions();
+    this.answerList = this.getAllAnswers();
+  }
 }
